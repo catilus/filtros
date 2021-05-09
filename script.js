114806ea5132c8f40ccfea8ae93543c0a4eec0cb
HelenePiet <helene.m.piet@gmail.com>
2021-05-09 14:51:06 -0700

    Added rainbow filter

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var originalImage = null;
 var grayImage = null;
 var redImage = null;
+var rainbowImage = null;
 var alphaImage = null;
 var canvas = document.getElementById("canvas");
 
@@ -8,6 +9,7 @@ function loadImage() {
   originalImage = new SimpleImage(document.getElementById("ogimage"));
   grayImage = new SimpleImage(document.getElementById("ogimage"));
   redImage = new SimpleImage(document.getElementById("ogimage"));
+  rainbowImage = new SimpleImage(document.getElementById("ogimage"));
   alphaImage = new SimpleImage(document.getElementById("ogimage"));
   
   originalImage.drawTo(canvas);
@@ -32,7 +34,7 @@ function makeGray() {
 
 function makeRed() {
   //alert("Red");
-  if (redImage == null || ! grayImage.complete()) {
+  if (redImage == null || ! redImage.complete()) {
     alert("Image has not loaded");
   }
   else {
@@ -88,7 +90,118 @@ function makeAlphaBorder() {
 }
 
 function makeRainbow() {
-  alert("Rainbow");
+  //alert("Rainbow");
+  var H = rainbowImage.getHeight();
+  
+  if (rainbowImage == null || ! rainbowImage.complete()) {
+    alert("Image has not loaded");
+  }
+  else {
+    for (var pixel of rainbowImage.values()){
+      var y = pixel.getY()
+      var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue())/3;
+      
+      // Red horizontal stripe
+      if (y <= H/7){
+        if (avg < 128) {
+            pixel.setRed(2*avg);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        }        
+        else {
+            pixel.setRed(255);
+            pixel.setGreen(2*avg-255);
+            pixel.setBlue(2*avg-255);
+        }
+      }
+
+      // Orange horizontal stripe
+      else if (y < H/7 || y <= 2*H/7 ){
+        if (avg < 128) {
+            pixel.setRed(2*avg);
+            pixel.setGreen(0.8*avg);
+            pixel.setBlue(0);
+        }        
+        else {
+            pixel.setRed(255);
+            pixel.setGreen(1.2*avg-51);
+            pixel.setBlue(2*avg-255);
+        }
+      }
+
+      // Yellow horizontal stripe
+      else if (y < 2*H/7 || y <= 3*H/7 ){
+        if (avg < 128) {
+            pixel.setRed(2*avg);
+            pixel.setGreen(2*avg);
+            pixel.setBlue(0);
+        }        
+        else {
+            pixel.setRed(255);
+            pixel.setGreen(255);
+            pixel.setBlue(2*avg-255);
+        }
+      }
+
+      // Green horizontal stripe
+      else if (y < 3*H/7 || y <= 4*H/7 ){
+        if (avg < 128) {
+            pixel.setRed(0);
+            pixel.setGreen(2*avg);
+            pixel.setBlue(0);
+        }        
+        else {
+            pixel.setRed(2*avg-255);
+            pixel.setGreen(255);
+            pixel.setBlue(2*avg-255);
+        }
+      }
+
+      // Blue horizontal stripe
+      else if (y < 4*H/7 || y <= 5*H/7 ){
+        if (avg < 128) {
+            pixel.setRed(0);
+            pixel.setGreen(0);
+            pixel.setBlue(2*avg);
+        }        
+        else {
+            pixel.setRed(2*avg-255);
+            pixel.setGreen(2*avg-255);
+            pixel.setBlue(255);
+        }
+      }
+
+      // Indigo horizontal stripe
+      else if (y < 5*H/7 || y <= 6*H/7 ){
+        if (avg < 128) {
+            pixel.setRed(0.8*avg);
+            pixel.setGreen(0);
+            pixel.setBlue(2*avg);
+        }        
+        else {
+            pixel.setRed(1.2*avg-51);
+            pixel.setGreen(2*avg-255);
+            pixel.setBlue(255);
+        }
+      }
+
+      // Purple horizontal stripe
+      else {
+        if (avg < 128) {
+            pixel.setRed(1.6*avg);
+            pixel.setGreen(0);
+            pixel.setBlue(1.6*avg);
+        }        
+        else {
+            pixel.setRed(0.4*avg+153);
+            pixel.setGreen(2*avg-255);
+            pixel.setBlue(0.4*avg+153);
+        }
+      }
+
+    }
+    rainbowImage.drawTo(canvas);
+  }
 }
 
 function resetImage() {
