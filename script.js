4ebe4cc1bca0f50a835b076e621f191f6b216e76
HelenePiet <helene.m.piet@gmail.com>
2021-05-09 12:57:38 -0700

    Added transparent border functionality.

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 var originalImage = null;
 var grayImage = null;
+var redImage = null;
+var alphaImage = null;
 var canvas = document.getElementById("canvas");
 
 function loadImage() {
   originalImage = new SimpleImage(document.getElementById("ogimage"));
   grayImage = new SimpleImage(document.getElementById("ogimage"));
   redImage = new SimpleImage(document.getElementById("ogimage"));
+  alphaImage = new SimpleImage(document.getElementById("ogimage"));
   
   originalImage.drawTo(canvas);
 }
@@ -34,21 +37,53 @@ function makeRed() {
   }
   else {
     for (var pixel of redImage.values()){
-        var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue())/3;
+      var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue())/3;
         
-        if (avg < 128) {
+      if (avg < 128) {
             pixel.setRed(2*avg);
             pixel.setGreen(0);
             pixel.setBlue(0);
-        }
+      }
         
-        else {
+      else {
             pixel.setRed(255);
             pixel.setGreen(2*avg-255);
             pixel.setBlue(2*avg-255);
-        }  
-}
+      }  
+    }
     redImage.drawTo(canvas);
+  }
+}
+
+function makeAlphaBorder() {
+  //alert("Alpha");
+  
+  if (alphaImage == null || ! alphaImage.complete()) {
+    alert("Image has not loaded");
+  }
+  else {
+    var imgWidth = alphaImage.getWidth();
+    var imgHeight = alphaImage.getHeight();
+    var borderSize = document.getElementById("sizeInput").value;
+    
+    if (isNaN(borderSize) || borderSize < 0) {
+      alert("Set a correct number.");
+    }   
+    else {  
+      for (var pixel of alphaImage.values()){
+        var x = pixel.getX();
+        var y = pixel.getY();
+      
+        if (x < borderSize || x > imgWidth-borderSize){
+          pixel.setAlpha(100);
+        }
+      
+        if (y < borderSize || y > imgHeight-borderSize){
+          pixel.setAlpha(100);
+        }
+      }
+      alphaImage.drawTo(canvas);
+    } 
   }
 }
 
